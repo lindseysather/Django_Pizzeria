8239d771a9a9eb8f7c9def983c4ec9de44f114af
lindseysather <lindsey_sather1@baylor.edu>
2021-12-10 12:20:36 -0600

    comments work!

--- pizzas/models.py
+++ pizzas/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Pizza(models.Model):
     name = models.CharField(max_length=200)
-    date_added = models.DateTimeField(auto_now_add=True)
+    date_added = models.DateTimeField(auto_now_add=True, null=True, blank=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     #shows text of name object:
@@ -16,7 +16,7 @@ class Toppings(models.Model):
     pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
     #name can hold values (pineapple, sausage, etc.)
     name = models.TextField()
-    date_added = models.DateTimeField(auto_now_add=True)
+    date_added = models.DateTimeField(auto_now_add=True, null=True, blank=True)
 
     #returns toppings (pineapple, ham) as text (instead of Topping Object (1))
     #string method
@@ -24,16 +24,15 @@ class Toppings(models.Model):
         return self.name
         #after we wrote this, we makemigrations and migrate because we change something 
 
-
 class Comment(models.Model):
     pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
     name = models.TextField()
-    date_added = models.DateTimeField(auto_now_add=True)
+    date_added = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+
 
     def __str__(self):
-        #returns first 50 characters of text
         return self.name
-     
+
 
 class Image(models.Model):
     title = models.CharField(max_length=200)
